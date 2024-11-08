40e2fba2c7c046cd044999d4c376324abb94a2e3
thilankas961 <thilankas961@gmail.com>
2024-11-08 11:28:03 +0530

    git ignore unnecessary files and directories

--- README.md
+++
@@ -1,1 +0,0 @@
-# Agent_Framework_Backend
--- main/coding/tmp_code_6c5331c38c065000d9fc854b7c37f541.sh
+++
@@ -1,7 +0,0 @@
-fls -o /dev/mapper/loop0p1 dfr-01-ntfs.dd > fls_output.txt
-ils -r | grep Deleted | awk '{print $3}' > deleted_inodes.txt
-ifind -i -o /dev/mapper/loop0p1 dfr-01-ntfs.dd < deleted_inodes.txt > ifind_output.txt
-awk '{print $3}' ifind_output.txt > deleted_files.txt
-rm fls_output.txt deleted_inodes.txt ifind_output.txt
-
-cat deleted_files.txt
