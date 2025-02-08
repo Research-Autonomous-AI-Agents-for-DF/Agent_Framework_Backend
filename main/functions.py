c0fb65ac690ce1a9b77d95e49195e7880fe15ca4
Luqman Fazal <luqman_fz@yahoo.com>
2025-02-09 00:01:00 +0530

    adding 404 handler and ask human function

--- main/functions.py
+++ main/functions.py
@@ -6,6 +6,13 @@ from bs4 import BeautifulSoup
 def get_tool_documentation(commandName:Annotated[str, "Sleuth Kit Command Line Tool Name"]) -> str:
     url = f"http://www.sleuthkit.org/sleuthkit/man/{commandName}.html"
     response = requests.get(url)
+    if response.status_code == 404:
+        return "Tool documentation not found"
     soup = BeautifulSoup(response.text, 'html.parser')
     body_content = soup.find('body')
-    return f"{body_content}"
+    return body_content.text
+def ask_human_expert(question:Annotated[str, "Question"]) -> str:
+    print("Please answer the following question:")
+    print(question)
+    answer = input("Your answer: ")
+    return answer
