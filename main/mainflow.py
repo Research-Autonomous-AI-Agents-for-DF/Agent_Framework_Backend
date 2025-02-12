7e14b6de2475375400e70b182584f80b2417aca1
Luqman Fazal <luqman_fz@yahoo.com>
2025-02-12 17:54:00 +0530

    Added taking task from user

--- main/mainflow.py
+++ main/mainflow.py
@@ -27,22 +27,17 @@ async def on_chat_start():
                 "This tool allows you to interact with AI-driven agents to perform various tasks."
     ).send()
 
-    action = await cl.AskActionMessage(
-        content="Click the button below to start.",
-        actions=[
-            cl.Action(name="start_chat", payload={"value": "start_chat"}, label="🚀 Start Chat")
-        ]
+    user_task = await cl.AskUserMessage(
+        content="Task to send to Agent Workflow",
+        author="User"
     ).send()
-
-    if action.get("name") == "start_chat":
-        await cl.Message(content=f"🔄 Starting agents on task: {TASK}...").send()
+    await cl.Message(content=f"🔄 Starting agents on task: {user_task.get("output")}...").send()
 
         # Se,perate process to start agents to avoid blocking the main process
-        asyncio.create_task(start_agents())
+    asyncio.create_task(start_agents(user_task.get("output")))
 
 
-async def start_agents():
-    await cl.Message(content=f"Starting agents on task: {TASK}...").send()
+async def start_agents(task):
     config_list = []
 
     for model in os.getenv("LLM_MODEL").split(","):
@@ -73,13 +68,34 @@ async def start_agents():
             Output one step at a time think about the result that is given to you and generate the next step.
 
             To analyze a disk image,
-            1. Identify the offsets for each partition (Write a bash code block using the mmls tool from TSK(The Sleuth Kit)).
-            2. Using the offsets, use a bash code block of the TSK tool to perform the task.
-
+            1. Identify the offsets for each partition (Suggest using the mmls tool from TSK(The Sleuth Kit)).
+            2. Using the offsets, suggest the most suitable TSK tool from the contextto perform the task.
+            
             Use the following structure to solve tasks:
                 1. **Thought**: Analyze the task and determine the best SleuthKit commands or sequence to solve it.
-                2. **Action**: Select the appropriate command(s) to use and justify your choice.
-                3. **Observation**: After performing the action, analyze the results. If further action is needed, continue with the next step.
+                2. **Action**: Select the appropriate command(s) and inputs required for the command(s) to use and justify your choice.
+                3. **Observation**: I will perform the action. Analyze the results. If further action is needed, continue with the next step.
+                
+            eg:
+            User's question is: What can you tell me about the partitions of the disk image?
+            image_location: ./dataset/my_image.dd
+            
+            Context is:<h1 id="volume-system-tools">Volume System Tools</h1>
+            <p>These tools take a disk (or other media) image as input and analyze its partition structures. Examples include DOS
+                partitions, BSD disk labels, and the Sun Volume Table of Contents (VTOC). These can be used find hidden data between
+                partitions and to identify the file system offset for The Sleuth Kit tools. The media management tools support DOS
+                partitions, BSD disk labels, Sun VTOC, and Mac partitions.</p>
+            <ul>
+                <li><strong>mmls</strong>: Displays the layout of a disk, including the unallocated spaces.</li>
+                <li><strong>mmstat</strong>: Display details about a volume system (typically only the type).</li>
+                <li><strong>mmcat</strong>: Extracts the contents of a specific volume to STDOUT.</li>
+            </ul>
+
+            **Thought**: To examine the partitions in the disk image, we need to use a tool that can give us partition data. The two relevant SleuthKit tools for this purpose are `mmls` and `mmstat`. Since `mmls` extracts the layout of the entire disk, we can use this first to get the partition details.
+
+            **Action**: Use `mmls` to extract the partition table from the disk image. You'll need image_location: ./dataset/my_image.dd as input
+
+            Please wait for further instructions based on the output of `mmls`.
             """
         ),
         llm_config=config_list[0],
@@ -94,10 +110,9 @@ async def start_agents():
         max_consecutive_auto_reply=3,
         retrieve_config={
             "task": "qa",
-            "docs_path": [os.path.join(os.path.abspath(""), "sleuthkit_commands.txt"),
-                          os.path.join(os.path.abspath(""), "tsk_Tool_Overview.html")],
-            "custom_text_types": ["txt", "html"],
-            "chunk_token_size": 100,
+            "docs_path": [os.path.join(os.path.abspath(""), "tsk_Tool_Overview.html")],
+            "custom_text_types": ["html"],
+            "chunk_token_size": 250,
             "model": llm_config["config_list"][0]["model"],
             "client": chromadb.PersistentClient(path="/tmp/chromadb"),
             "embedding_model": "all-mpnet-base-v2",
@@ -114,19 +129,24 @@ async def start_agents():
         llm_config=config_list[0],
         code_execution_config=False,
         human_input_mode="ALWAYS",
-        system_message="""You are a helpful AI assistant.
-    Solve tasks using your coding and language skills.
-    In the following case, suggest a shell script (in a bash coding block) for the user to execute.
+                system_message="""You are a helpful AI assistant.
+    I will suggest you with a command from the sleuth kit (TSK). Solve tasks using your coding and language skills.
+    In the following case, suggest code (in a bash coding block) for the user to execute.
         1. When you need to perform some task with code, use the code to perform the task and output the result. Finish the task smartly.
+        2. When you need to collect info, use the code to output the info you need, for example, browse or search the web, download/read a file, print the content of a webpage or a file, get the current date/time, check the operating system. After sufficient info is printed and the task is ready to be solved based on your language skill, you can solve the task by yourself.
     Solve the task step by step if you need to. If a plan is not provided, explain your plan first. Be clear which step uses code, and which step uses your language skill.
     When using code, you must indicate the script type in the code block. The user cannot provide any other feedback or perform any other action beyond executing the code you suggest. The user can't modify your code. So do not suggest incomplete code which requires users to modify. Don't use a code block if it's not intended to be executed by the user.
     If you want the user to save the code in a file before executing it, put # filename: <filename> inside the code block as the first line. Don't include multiple code blocks in one response. Do not ask users to copy and paste the result. Instead, use 'print' function for the output when relevant. Check the execution result returned by the user.
     If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info by asking the user if you need, and think of a different approach to try.
     When you find an answer, verify the answer carefully. Include verifiable evidence in your response if possible.
+    
+    Important
+    1. Do one command at a time.
+    2. If you are generating a shell script, dont have any blank lines as it gets interpreted as \r in the terminal
 
-    Call get_tool_documentation to get the documentation for a sleuth kit command line tool.
+    Call get_tool_documentation to get the documentation for a TSK command line tool.
     Reply "TERMINATE" in the end when everything is done.
-        """
+"""
     )
 
     context_handling = transform_messages.TransformMessages(
@@ -189,7 +209,7 @@ async def start_agents():
         if last_speaker is task_translation_agent:
             if "UPDATE CONTEXT" in messages[-1]["content"]:
                 return rag_proxy_agent;
-            return code_executor_agent
+            return coder_agent
         # elif last_speaker is code_executor_agent:
         #     # Check if execution was successful or failed
         #     if "execution failed" in messages[-1]["content"] or "failed" in messages[-1]["content"] or "change" in messages[-1]["content"] or "Error" in messages[-1]["content"]:
@@ -198,9 +218,9 @@ async def start_agents():
         #     # If successful, go back to Task Translation Agent for the next task
         #     if "execution successful" in messages[-1]["content"] or "success" in messages[-1]["content"] or "succeed" in messages[-1]["content"]:
         #         return task_translation_agent
-        elif last_speaker is coder_agent:
-            # If coder agent is used, go back to Task Translation Agent for the next task
-            return task_translation_agent
+        # elif last_speaker is coder_agent:
+        #     # If coder agent is used, go back to Task Translation Agent for the next task
+        #     return task_translation_agent
         # elif last_speaker is reporter_agent:
         #     # Once all tasks are completed, switch to manual mode for final review
         #     return "manual"
@@ -238,5 +258,5 @@ async def start_agents():
     await cl.make_async(rag_proxy_agent.initiate_chat)(
         manager,
         message=rag_proxy_agent.message_generator,
-        problem=TASK
+        problem=task
     )
